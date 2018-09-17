bc33a686fe492a6429d7b5f9c4f86485646d9868
Vajira <vajirawke@gmail.com>
2018-09-17 15:13:21 +0530

    sub category search

--- search/src/main/java/com/ranpatha/search/SearchApplication.java
+++ search/src/main/java/com/ranpatha/search/SearchApplication.java
@@ -1,10 +1,20 @@
 package com.ranpatha.search;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories({"com.ranpatha.search.service"})
+
 public class SearchApplication {
+	
+	@Autowired
+	  private MongoTemplate mongoTemplate;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SearchApplication.class, args);
